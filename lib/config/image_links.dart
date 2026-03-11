1337ea99df467710bdbb394b4fece7a51e4f16df
Aruna <admgbit@gmail.com>
2026-03-12 00:06:13 +0530

    citizen dashboard

--- lib/config/image_links.dart
+++ lib/config/image_links.dart
@@ -2,4 +2,16 @@ class ImageLinks {
   static const roleSelectorScreenImage = 'https://media.istockphoto.com/id/1460660429/photo/hand-holding-the-earth-on-a-green-background-with-icon-recycle-in-the-concept-of-reuse-reduce.jpg?s=612x612&w=0&k=20&c=s2xFTU02ZCT_RmZoyurWOwRj0Bpzi6jPM4Kc5yWwEpo=';
   static const loginScreenImage = 'https://media.istockphoto.com/id/1460660429/photo/hand-holding-the-earth-on-a-green-background-with-icon-recycle-in-the-concept-of-reuse-reduce.jpg?s=612x612&w=0&k=20&c=s2xFTU02ZCT_RmZoyurWOwRj0Bpzi6jPM4Kc5yWwEpo=';
   static const citizenDashboardScreenImage = 'https://media.istockphoto.com/id/1460660429/photo/hand-holding-the-earth-on-a-green-background-with-icon-recycle-in-the-concept-of-reuse-reduce.jpg?s=612x612&w=0&k=20&c=s2xFTU02ZCT_RmZoyurWOwRj0Bpzi6jPM4Kc5yWwEpo=';
+
+  // Citizen
+  static const citizenDashboardComplain = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAztjE3rbA5rcEG_peduiyHZFt-BEItEU8dg&s';
+  static const citizenDashboardTrackComplain = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGiRjYq9u0IpjCTjinO-_ASPJj7R2GYnqR8g&s';
+  static const citizenDashboardNewVehicleRoutes = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROBcSVT580WC8bqfmd8dzuCcxp4EpNgOsOcw&s';
+  static const citizenDashboardNewBin = 'https://ekombud.com/wp-content/uploads/2020/01/kontener-mulda-7m3-asymetryczna-ekombud-a1-1024x527.jpg';
+  static const citizenDashboardCollectionSchedule = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrMOfbRmCj_pmMDidp9-pnedPEvs2Hr9G9xQ&s';
+  static const citizenDashboardNotification = 'https://media.kasperskydaily.com/wp-content/uploads/sites/92/2021/05/07113722/annoying-notifications-android-featured.jpg';
+
+  static const citizenDashboardPolicyUpdates = 'https://img.freepik.com/premium-photo/policies-update-message-written-white-paper-with-blurry-lake-background-conceptual-policies-update-symbol-copy-space_648871-12774.jpg?semt=ais_hybrid&w=740&q=80';
+
+  static const citizenDashboardEnvironmentalLaw = 'https://www.lloydlawcollege.edu.in/images/LLC/website/images/blog_img/environmental-law.webp';
 }
--- lib/viewmodels/citizen/citizen_dashboard_provider.dart
+++ lib/viewmodels/citizen/citizen_dashboard_provider.dart
@@ -10,17 +10,17 @@ class DashboardItem {
 
 final citizenDashboardItemsProvider = Provider<List<DashboardItem>>((ref) {
   return [
-    DashboardItem(title: "Complaint", imagePath: ImageLinks.loginScreenImage),
-    DashboardItem(title: "Track Complaints", imagePath: ImageLinks.loginScreenImage),
-    DashboardItem(title: "New Vehicle Routes", imagePath: ImageLinks.loginScreenImage),
-    DashboardItem(title: "New Bin", imagePath: ImageLinks.loginScreenImage),
-    DashboardItem(title: "Collection Schedule", imagePath: ImageLinks.loginScreenImage),
-    DashboardItem(title: "Notification", imagePath: ImageLinks.loginScreenImage),
+    DashboardItem(title: "Complaint", imagePath: ImageLinks.citizenDashboardComplain),
+    DashboardItem(title: "Track Complaints", imagePath: ImageLinks.citizenDashboardTrackComplain),
+    DashboardItem(title: "New Vehicle Routes", imagePath: ImageLinks.citizenDashboardNewVehicleRoutes),
+    DashboardItem(title: "New Bin", imagePath: ImageLinks.citizenDashboardNewBin),
+    DashboardItem(title: "Collection Schedule", imagePath: ImageLinks.citizenDashboardCollectionSchedule),
+    DashboardItem(title: "Notification", imagePath: ImageLinks.citizenDashboardNotification),
     DashboardItem(title: "User Reports", imagePath: ImageLinks.loginScreenImage),
     DashboardItem(title: "Recycling Tips", imagePath: ImageLinks.loginScreenImage),
     DashboardItem(title: "Waste Statistics", imagePath: ImageLinks.loginScreenImage),
     DashboardItem(title: "Policy Updates", imagePath: ImageLinks.loginScreenImage),
     DashboardItem(title: "Environmental Impact", imagePath: ImageLinks.loginScreenImage),
-    DashboardItem(title: "Environmental Law", imagePath: ImageLinks.loginScreenImage),
+    DashboardItem(title: "Environmental Law", imagePath: ImageLinks.citizenDashboardEnvironmentalLaw),
   ];
 });
